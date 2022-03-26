29e11765b6b113230876630b2f3d431f5d3d1aa1
Harshbir Singh <singh.hveer1@gmail.com>
2022-03-26 02:28:27 -0600

    Implement code for character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", counter);
+});
+
+
+function counter() {
+  console.log($(this).val().length);
+  let  countNumberEl = $($(this).parent().children()[1]).children()[1]; 
+
+  let count = 140;
+  count = 140 - $(this).val().length;
+
+  if(count >= 0){
+    $(countNumberEl).removeClass("red-counter");
+    $(countNumberEl).text(count);
+  } else {
+    $(countNumberEl).addClass("red-counter");
+    $(countNumberEl).text(count);
+  }
+
+  
+}
