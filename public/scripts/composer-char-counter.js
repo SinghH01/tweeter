f42a93074b3d0172211f96c5a253ffcc5c2a8cf8
Harshbir Singh <singh.hveer1@gmail.com>
2022-03-31 01:58:44 -0600

    ESLint

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,18 +5,16 @@ $(document).ready(function() {
 
 function counter() {
   console.log($(this).val().length);
-  let  countNumberEl = $($(this).parent().children()[1]).children()[1]; 
+  let  countNumberEl = $($(this).parent().children()[1]).children()[1];
 
   let count = 140;
   count = 140 - $(this).val().length;
 
-  if(count >= 0){
+  if (count >= 0) {
     $(countNumberEl).removeClass("red-counter");
     $(countNumberEl).text(count);
   } else {
     $(countNumberEl).addClass("red-counter");
     $(countNumberEl).text(count);
-  }
-
-  
+  }  
 }
